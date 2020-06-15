e8139486f13f1eb08546be87dd11380e14f9d0eb
Idan <idan.zonshein@powerhrg.com>
2020-06-15 13:30:09 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :phone_number, :presence => true,
+                           :numericality => true,
+                           :length => { :minimum => 10, :maximum => 15 }
 end
